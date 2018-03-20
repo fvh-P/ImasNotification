bbe19a3cf61658bd672feff2b073ddd1731c5dc3
fvh_P <ko_kurihara@yahoo.co.jp>
2018-03-21 00:40:03 +0900

    cut out a class to a file

--- ImasNotification/Program.cs
+++ ImasNotification/Program.cs
@@ -7,71 +7,11 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Mastonet;
 using HtmlAgilityPack;
 using System.Collections.ObjectModel;
 
 namespace ImasNotification
 {
-    class FeedList : List<long>
-    {
-        public FeedList() : base() { }
-        public FeedList(int capacity) : base(capacity) { }
-
-        public void Subscribe(PostManager pm, string from, long id, long accountId, Visibility v)
-        {
-            var content = $"@{from} ";
-            if (Contains(accountId))
-            {
-                content += $"すでに購読済みです。\n";
-            }
-            else
-            {
-                Add(accountId);
-                if (File.Exists("feedList.json"))
-                {
-                    File.Delete("feedList.json");
-                }
-                File.WriteAllLines("feedList.json", this.Select(x => x.ToString()));
-                content += $"購読登録しました。\n" +
-                    $"アイマスニュースおよびアイマス公式ブログ更新情報をDMで配信します。" +
-                    $"解除する場合はinfo宛にfeed removeとリプライしてください。";
-            }
-            pm.Col.Add(new PostContent(id, content, false, null, v: v));
-        }
-
-        public void UnSubscribe(PostManager pm, string from, long id, long accountId, Visibility v)
-        {
-            var content = $"@{from} ";
-            if (Contains(accountId))
-            {
-                Remove(accountId);
-                if (File.Exists("feedList.json"))
-                {
-                    File.Delete("feedList.json");
-                }
-                File.WriteAllLines("feedList.json", this.Select(x => x.ToString()));
-                content += $"購読解除しました。\n";
-            }
-            else
-            {
-                content += $"購読していません。\n";
-            }
-            pm.Col.Add(new PostContent(id, content, false, null, v: v));
-        }
-
-        public void ShowHelp(PostManager pm, string from, long id, Visibility v)
-        {
-            var content = $"feedコマンド アイマスニュースとアイマス公式ブログの更新情報をDMでお知らせします。\n" +
-                    $"使い方  (at)はアットマーク\n\n" +
-                    $"(at)info feed add\n" +
-                    $"  購読します。以降更新情報があるとDMで送られてきます。\n\n" +
-                    $"(at)info feed remove\n" +
-                    $"  購読を解除します\n";
-            pm.Col.Add(new PostContent(id, content, true, "feedコマンドのヘルプ\n", v: v));
-            return;
-        }
-    }
     class Program
     {
         static DailyJobList dailyJobList;
